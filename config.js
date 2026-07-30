b5886694f9c537ab641aecada3a9109f60d38282
Kadthicu <dima72orlov@gmail.com>
2026-07-30 17:09:42 +0500

    feat: add centralized gift configuration

---
+++ config.js
@@ -0,0 +1,9 @@
+window.DRAGON_GIFT_CONFIG = Object.freeze({
+  couple: {
+    names: "Антонина и Алексей",
+    yearsTogether: 27,
+    address: "Дорогие мама и папа!"
+  },
+  signature: "Крепко обнимаем и очень любим вас.\nВаши дети ❤️",
+  storageKey: "dragon-gift-v3-progress"
+});
